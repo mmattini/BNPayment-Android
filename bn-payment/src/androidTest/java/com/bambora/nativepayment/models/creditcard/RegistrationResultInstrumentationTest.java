fae13cfb738d4252ea4eb0803454456b003b4b21
Lovisa Corp <lovisa.corp@bambora.com>
2016-08-26 09:25:59 +0200

    PG-620 Add test.

--- bn-payment/src/androidTest/java/com/bambora/nativepayment/models/creditcard/RegistrationResultInstrumentationTest.java
+++ bn-payment/src/androidTest/java/com/bambora/nativepayment/models/creditcard/RegistrationResultInstrumentationTest.java
@@ -31,6 +31,14 @@ import org.json.JSONException;
  */
 public class RegistrationResultInstrumentationTest extends InstrumentationTestCase {
 
+    private static final String KEY_CARD_NUMBER = "truncatedcardnumber";
+    private static final String KEY_EXPIRY_MONTH = "expmonth";
+    private static final String KEY_EXPIRY_YEAR = "expyear";
+    private static final String KEY_PAYMENT_TYPE = "paymenttype";
+    private static final String KEY_TRANSACTION_ID = "transactionid";
+    private static final String KEY_SUBSCRIPTION_ID = "subscriptionid";
+    private static final String KEY_ORIGIN_IP = "originip";
+
     public void testFromJsonWithoutParameters() throws JSONException {
         // Given
         String resultJson = "{}";
@@ -47,5 +55,36 @@ public class RegistrationResultInstrumentationTest extends InstrumentationTestCa
         assertNull(registrationResult.transactionId);
         assertNull(registrationResult.subscriptionId);
         assertNull(registrationResult.originIp);
+    }
+
+    public void testFromJsonWithValidParameters() throws JSONException {
+        // Given
+        String cardNumber = "123456789";
+        Integer expiryMonth = 1;
+        Integer expiryYear = 2;
+        String paymentType = "payment-type";
+        String transactionId = "transaction-id";
+        String subscriptionId = "subscription-id";
+        String originIp = "origin-ip";
+        String resultJson = "{" +
+                KEY_CARD_NUMBER + ":" + cardNumber + "," +
+                KEY_EXPIRY_MONTH + ":" + expiryMonth + "," +
+                KEY_EXPIRY_YEAR + ":" + expiryYear + "," +
+                KEY_PAYMENT_TYPE + ":" + paymentType + "," +
+                KEY_TRANSACTION_ID + ":" + transactionId + "," +
+                KEY_SUBSCRIPTION_ID + ":" + subscriptionId + "," +
+                KEY_ORIGIN_IP + ":" + originIp + "}";
+
+        // When
+        RegistrationResult registrationResult = RegistrationResult.fromJson(resultJson);
+
+        // Then
+        assertEquals(cardNumber, registrationResult.truncatedCardNumber);
+        assertEquals(expiryMonth, registrationResult.expiryMonth);
+        assertEquals(expiryYear, registrationResult.expiryYear);
+        assertEquals(paymentType, registrationResult.paymentType);
+        assertEquals(transactionId, registrationResult.transactionId);
+        assertEquals(subscriptionId, registrationResult.subscriptionId);
+        assertEquals(originIp, registrationResult.originIp);
     }
 }
